907997ae49b85d6bfe6853a78ac301a3106f0a62
Colin Cummins <all.our.august.sundays@gmail.com>
2026-01-14 16:38:02 -0800

    Basic tcp listener established

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use::tokio::net::{TcpListener, TcpStream};
+use::tokio::io::AsyncWriteExt;
+use std::io; 
+const ADDR: &str = "127.0.0.1:6379";
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind(ADDR).await?;
+
+    loop {
+        match listener.accept().await {
+            Ok((mut _socket, addr)) => {
+                println!("new client: {:?}", addr);
+                _socket.write_all(b"Connected").await?},
+            Err(e) => println!("client couldn't connect: {:?}", e)
+        }
+    }
+
 }
