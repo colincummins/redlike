04f5aed4cc5ac8caad29cf1fbf4d9261f44f0330
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2026-01-15 18:46:38 -0800

    handle_connection work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use tokio::io::AsyncReadExt;
 use::tokio::net::{TcpListener, TcpStream};
 use::tokio::io::AsyncWriteExt;
 use std::io; 
 const ADDR: &str = "127.0.0.1:6379";
+const READ_BUFFER_SIZE: usize = 1024;
+
+async fn handle_connection(mut socket: TcpStream) -> io::Result<()> {
+
+    loop {
+        let mut read_buffer: [u8; READ_BUFFER_SIZE] = [0; READ_BUFFER_SIZE];
+        socket.read_buf(read_buffer);
+
+    }
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -11,7 +22,8 @@ async fn main() -> io::Result<()> {
         match listener.accept().await {
             Ok((mut _socket, addr)) => {
                 println!("new client: {:?}", addr);
-                _socket.write_all(b"Connected").await?},
+                handle_connection(_socket).await?;
+            },
             Err(e) => println!("client couldn't connect: {:?}", e)
         }
     }
