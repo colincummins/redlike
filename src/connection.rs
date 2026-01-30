f278db3f2bf34231e9c0a04052fbd77e7ec8c9bf
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2026-01-29 18:29:57 -0800

    Unit test for end of line in connection

--- src/connection.rs
+++ src/connection.rs
@@ -87,6 +87,14 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn end_of_line () {
+        let store:Store = Default::default();
+        let mut connection: Connection<tokio::io::Empty, _> = Connection::new(tokio::io::empty(), sink(), store);
+        let cmd = connection.read_command().await.unwrap();
+        assert_eq!(cmd, None);
+    }
+
+    #[tokio::test]
     async fn successful_read_ping () {
         let (mut connection, mut client) = setup_connection();
         client.write_all(b"PING\n").await.unwrap();
