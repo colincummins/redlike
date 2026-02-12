0f61bb282353c8a8ac65ce569a8ab66fb830ebeb
Colin Cummins <84826067+colincummins@users.noreply.github.com>
2026-02-11 16:04:59 -0800

    Store (#13)
    Implement Store library. Simple wrapper for our shared hashmap with get/set/delete and unit testing for same.

--- src/store.rs
+++ src/store.rs
@@ -1,5 +1,76 @@
 use std::collections::HashMap;
+use std::default;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
-pub type Store= Arc<RwLock<HashMap<String, String>>>;
+pub struct Store {
+    inner: Arc<RwLock<HashMap<String, String>>>
+}
+
+impl Store {
+    pub fn new() -> Store{
+        Store { inner: Arc::new(RwLock::new(HashMap::new())) }
+    }
+
+    pub async fn get(&self, key: &str) -> Option<String>{
+        let map = self.inner.read().await;
+        map.get(key).cloned()
+    }
+
+    pub async fn set(&self, key: String, value: String) -> Option<String>{
+        let mut map = self.inner.write().await;
+        map.insert(key, value)
+    }
+
+    pub async fn delete(&self, key: &str) -> Option<String>{
+        let mut map = self.inner.write().await;
+        map.remove(key)
+    }
+
+} 
+
+impl Clone for Store {
+    fn clone(&self) -> Self {
+        Store {
+            inner: self.inner.clone()
+        }
+    }
+}
+
+impl Default for Store {
+    fn default() -> Self {
+        Store::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn set_then_get() {
+        let store = Store::new();
+        store.set("newkey".to_string(), "newvalue".to_string()).await;
+        assert_eq!(Some("newvalue".to_string()),store.get("newkey").await)
+    }
+
+    #[tokio::test]
+    async fn get_nonexistent_key() {
+        let store = Store::new();
+        assert_eq!(None, store.get("newkey").await)
+    }
+
+    #[tokio::test]
+    async fn delete_existing_key() {
+        let store = Store::new();
+        store.set("newkey".to_string(), "newvalue".to_string()).await;
+        assert!(store.delete("newkey").await.is_some())
+    }
+
+    #[tokio::test]
+    async fn delete_nonexistent_key() {
+        let store = Store::new();
+        assert!(store.delete("newkey").await.is_none())
+    }
+
+}
