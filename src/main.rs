0f342c1cf7783e2308a4d594cb31de13387bbdf9
Colin Cummins <all.our.august.sundays@gmail.com>
2026-02-17 15:55:14 -0800

    TCP listener loop running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use redlike::connection::{self, Connection};
 use tokio::net::{TcpListener};
 use tokio::io::Result;
 use redlike::store::Store;
@@ -9,12 +10,21 @@ const ADDR: &str = "127.0.0.1:6379";
 #[allow(unused_variables)]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind(ADDR).await?;
-    let store: Store;
+    let store = Store::new();
 
     loop {
         match listener.accept().await {
-            Ok((mut _socket, addr)) => {
-                println!("new client: {:?}", addr);
+            Ok((mut socket, _addr)) => {
+                    let store = store.clone();
+                let handle = tokio::spawn(
+                    async move {
+                        let (read_half, write_half) = socket.split();
+                        let mut conn = Connection::new(read_half, write_half, store);
+                        if let Err(e) = conn.run().await {
+                            println!("connection failed: {:?}", e)
+                        }
+                    }
+                );
             },
             Err(e) => println!("client couldn't connect: {:?}", e)
         }
