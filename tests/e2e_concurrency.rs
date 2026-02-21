087fa56da3ca83661c7d327d697da83be99b36ee
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2026-02-21 11:03:30 -0800

    e2e concurrency testing scaffolding

--- tests/e2e_concurrency.rs
+++ tests/e2e_concurrency.rs
@@ -1,5 +1,11 @@
 mod common;
-use common::setup_test_server_and_test_client::setup_test_server_and_test_client;
 use common::test_case::TestCase;
-use redlike::server::server_from_listener;
+use redlike::server::{run_server, server_from_listener};
 const ADDR: &str = "127.0.0.1:0";
+
+#[tokio::test]
+async fn concurrent_get_set_del() -> tokio::io::Result<()> {
+    let (handle, addr) = run_server(ADDR).await?;
+
+    Ok(())
+}
