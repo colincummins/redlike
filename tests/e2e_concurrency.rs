c8d4f7f2a2a080a8b4c162ddf1813b4708864890
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2026-02-22 14:53:09 -0800

    Single key concurrency testing complete

--- tests/e2e_concurrency.rs
+++ tests/e2e_concurrency.rs
@@ -1,20 +1,32 @@
 mod common;
-use std::{net::ToSocketAddrs, os::unix::net::SocketAddr};
-
-use common::test_case::TestCase;
 use common::test_client::TestClient;
 use redlike::server::{run_server, server_from_listener};
+use tokio::task::JoinSet;
 const ADDR: &str = "127.0.0.1:0";
 
 async fn test_all_commands(addr: std::net::SocketAddr) -> tokio::io::Result<()> {
     let mut client = TestClient::new(addr).await?;
 
-    for _ in 0..100 {
+    for i in 0..10 {
+        println!("Spawning client...");
         client.write("PING\n").await?;
+        tokio::time::sleep(tokio::time::Duration::from_millis(i * 10)).await;
         assert_eq!(client.read_line().await?, "PONG\n".to_string());
+        client.write("SET mykey myvalue\n").await?;
+        tokio::time::sleep(tokio::time::Duration::from_millis(i * 10)).await;
+        assert!(matches!(client.read_line().await?.as_ref(), "OK\n"));
+        client.write("GET mykey\n").await?;
+        tokio::time::sleep(tokio::time::Duration::from_millis(i * 10)).await;
+        assert!(matches!(
+            client.read_line().await?.as_ref(),
+            "myvalue\n" | "\n"
+        ));
+        client.write("DEL mykey\n").await?;
+        tokio::time::sleep(tokio::time::Duration::from_millis(i * 10)).await;
+        assert!(matches!(client.read_line().await?.as_ref(), "0\n" | "1\n"));
     }
 
-    client.write("QUIT\n").await?;
+    client.send_quit().await?;
 
     Ok(())
 }
@@ -23,7 +35,15 @@ async fn test_all_commands(addr: std::net::SocketAddr) -> tokio::io::Result<()> 
 async fn get_set_del_same_record() -> tokio::io::Result<()> {
     let (addr, handle) = run_server(ADDR).await?;
 
-    test_all_commands(addr).await?;
+    let mut client_handles = JoinSet::new();
+
+    for _i in 0..32 {
+        client_handles.spawn(test_all_commands(addr));
+    }
+
+    while let Some(res) = client_handles.join_next().await {
+        res??;
+    }
 
     handle.abort();
     Ok(())
