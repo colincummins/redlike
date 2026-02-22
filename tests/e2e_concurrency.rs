3ed4e6b7e8fc76d785cd48dfc839f8cb0d36ef64
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2026-02-21 16:29:42 -0800

    wip/testing: building concurrency testing

--- tests/e2e_concurrency.rs
+++ tests/e2e_concurrency.rs
@@ -1,11 +1,30 @@
 mod common;
+use std::{net::ToSocketAddrs, os::unix::net::SocketAddr};
+
 use common::test_case::TestCase;
+use common::test_client::TestClient;
 use redlike::server::{run_server, server_from_listener};
 const ADDR: &str = "127.0.0.1:0";
 
-#[tokio::test]
-async fn concurrent_get_set_del() -> tokio::io::Result<()> {
-    let (handle, addr) = run_server(ADDR).await?;
+async fn test_all_commands(addr: std::net::SocketAddr) -> tokio::io::Result<()> {
+    let mut client = TestClient::new(addr).await?;
 
+    for _ in 0..100 {
+        client.write("PING\n").await?;
+        assert_eq!(client.read_line().await?, "PONG\n".to_string());
+    }
+
+    client.write("QUIT\n").await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn get_set_del_same_record() -> tokio::io::Result<()> {
+    let (addr, handle) = run_server(ADDR).await?;
+
+    test_all_commands(addr).await?;
+
+    handle.abort();
     Ok(())
 }
