2f16700972e59de6d29241e488ab4d6957101635
Colin Cummins <all.our.august.sundays@gmail.com>
2026-02-24 16:52:54 -0800

    Implement simple string parsing, implement unit testing for same

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,4 @@
-use std::string::ParseError;
-
+#[derive(Debug, PartialEq)]
 enum Frame {
     SimpleString(String),
     SimpleError(String),
@@ -7,6 +6,7 @@ enum Frame {
     Array(Option<Vec<Frame>>),
 }
 
+#[derive(Debug, PartialEq)]
 enum State {
     Start,
     ReadingLine,
@@ -15,20 +15,98 @@ enum State {
     ReadingArray,
 }
 
+#[derive(Debug, PartialEq)]
+enum ParseError {
+    ParseStrError,
+}
+
+#[derive(Debug, PartialEq)]
 struct Parser {
     state: State,
 }
 
 impl Parser {
-    fn parse(&self, buf: &[u8]) -> Result<Option<(Frame, usize)>, ParseError> {
+    fn new() -> Self {
+        Parser {
+            state: State::Start,
+        }
+    }
+
+    fn parse<'a>(&self, buf: &'a [u8]) -> Result<Option<(Frame, &'a [u8])>, ParseError> {
         match self.state {
             State::Start => {
-                if let Some(first_character) = buf.get(0) {
+                if let Some(first) = buf.get(0) {
+                    match first {
+                        b'+' => {
+                            let pos = match buf.windows(2).position(|w| w == b"\r\n") {
+                                Some(pos) => pos,
+                                None => return Ok(None),
+                            };
+                            let payload = std::str::from_utf8(&buf[1..pos])
+                                .map_err(|_| ParseError::ParseStrError)?;
+                            return Ok(Some((
+                                Frame::SimpleString(payload.to_owned()),
+                                &buf[pos + 2..],
+                            )));
+                        }
+                        _ => return Ok(None),
+                    }
                 } else {
                     return Ok(None);
                 }
             }
+            _ => return Ok(None),
         }
-        Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn incomplete_just_a_plus() {
+        let p = Parser::new();
+        let buf = b"+";
+        assert_eq!(p.parse(buf), Ok(None));
+    }
+
+    #[test]
+    fn incomplete_plus_and_ok() {
+        let p = Parser::new();
+        let buf = b"+OK";
+        assert_eq!(p.parse(buf), Ok(None));
+    }
+
+    #[test]
+    fn empty_simple_string() {
+        let p = Parser::new();
+        let buf = b"+\r\n";
+        assert_eq!(
+            p.parse(buf),
+            Ok(Some((Frame::SimpleString("".to_string()), &b""[..])))
+        )
+    }
+
+    #[test]
+    fn parse_simple_string() {
+        let p = Parser::new();
+        let buf = b"+OK\r\n";
+        assert_eq!(
+            p.parse(buf),
+            Ok(Some((Frame::SimpleString("OK".to_string()), &b""[..])))
+        )
+    }
+
+    #[test]
+    fn only_grabs_one_frame() {
+        let p = Parser::new();
+        let buf = b"+OK\r\n+OK\r\n";
+        assert_eq!(
+            p.parse(buf),
+            Ok(Some((
+                Frame::SimpleString("OK".to_string()),
+                &b"+OK\r\n"[..]
+            )))
+        )
     }
 }
