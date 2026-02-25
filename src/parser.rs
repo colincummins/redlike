be68ac480b10685120cb04ca72207e2257c8a635
Colin Cummins <all.our.august.sundays@gmail.com>
2026-02-25 09:34:05 -0800

    wip/fet/resp_parser

--- src/parser.rs
+++ src/parser.rs
@@ -2,17 +2,19 @@
 enum Frame {
     SimpleString(String),
     SimpleError(String),
-    Bulk(Option<String>),
+    Bulk(Option<Vec<u8>>),
     Array(Option<Vec<Frame>>),
 }
 
 #[derive(Debug, PartialEq)]
 enum State {
     Start,
+    ReadingSimpleString,
     ReadingLine,
-    ReadingBulk,
-    ReadingSize,
-    ReadingArray,
+    ReadingBulkLen,
+    ReadingBulk(usize),
+    ReadingArraySize,
+    ReadingArray(usize, Vec<Frame>),
 }
 
 #[derive(Debug, PartialEq)]
@@ -32,30 +34,34 @@ impl Parser {
         }
     }
 
-    fn parse<'a>(&self, buf: &'a [u8]) -> Result<Option<(Frame, &'a [u8])>, ParseError> {
-        match self.state {
-            State::Start => {
-                if let Some(first) = buf.get(0) {
-                    match first {
-                        b'+' => {
-                            let pos = match buf.windows(2).position(|w| w == b"\r\n") {
-                                Some(pos) => pos,
-                                None => return Ok(None),
-                            };
-                            let payload = std::str::from_utf8(&buf[1..pos])
-                                .map_err(|_| ParseError::ParseStrError)?;
-                            return Ok(Some((
-                                Frame::SimpleString(payload.to_owned()),
-                                &buf[pos + 2..],
-                            )));
-                        }
-                        _ => return Ok(None),
+    fn parse<'a>(&mut self, buf: &'a [u8]) -> Result<Option<(Frame, &'a [u8])>, ParseError> {
+        loop {
+            match self.state {
+                State::Start => match buf.first() {
+                    Some(b'+') => {
+                        self.state = State::ReadingSimpleString;
+                        continue;
                     }
-                } else {
-                    return Ok(None);
+                    Some(_) => return Ok(None),
+                    None => return Ok(None),
+                },
+
+                State::ReadingSimpleString => {
+                    let pos = match buf.windows(2).position(|w| w == b"\r\n") {
+                        Some(pos) => pos,
+                        None => return Ok(None),
+                    };
+                    let payload =
+                        std::str::from_utf8(&buf[1..pos]).map_err(|_| ParseError::ParseStrError)?;
+                    self.state = State::Start;
+                    return Ok(Some((
+                        Frame::SimpleString(payload.to_owned()),
+                        &buf[pos + 2..],
+                    )));
                 }
+
+                _ => return Ok(None),
             }
-            _ => return Ok(None),
         }
     }
 }
@@ -65,21 +71,21 @@ mod tests {
     use super::*;
     #[test]
     fn incomplete_just_a_plus() {
-        let p = Parser::new();
+        let mut p = Parser::new();
         let buf = b"+";
         assert_eq!(p.parse(buf), Ok(None));
     }
 
     #[test]
     fn incomplete_plus_and_ok() {
-        let p = Parser::new();
+        let mut p = Parser::new();
         let buf = b"+OK";
         assert_eq!(p.parse(buf), Ok(None));
     }
 
     #[test]
     fn empty_simple_string() {
-        let p = Parser::new();
+        let mut p = Parser::new();
         let buf = b"+\r\n";
         assert_eq!(
             p.parse(buf),
@@ -89,7 +95,7 @@ mod tests {
 
     #[test]
     fn parse_simple_string() {
-        let p = Parser::new();
+        let mut p = Parser::new();
         let buf = b"+OK\r\n";
         assert_eq!(
             p.parse(buf),
@@ -99,7 +105,7 @@ mod tests {
 
     #[test]
     fn only_grabs_one_frame() {
-        let p = Parser::new();
+        let mut p = Parser::new();
         let buf = b"+OK\r\n+OK\r\n";
         assert_eq!(
             p.parse(buf),
