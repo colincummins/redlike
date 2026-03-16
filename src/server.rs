14821064c256b34c717eb368f7ea420f1724d1ba
Colin Cummins <all.our.august.sundays@gmail.com>
2026-03-16 12:54:13 -0700

    Server now keeps track of open connections so we can issue graceful shutdown orders later

--- src/server.rs
+++ src/server.rs
@@ -4,24 +4,35 @@ use crate::connection::Connection;
 use crate::store::Store;
 use tokio::io::Result;
 use tokio::net::TcpListener;
-use tokio::task::JoinHandle;
+use tokio::select;
+use tokio::task::{JoinHandle, JoinSet};
 
 pub async fn server_from_listener(listener: TcpListener) -> Result<()> {
     let store = Store::new();
+    let mut open_connections = JoinSet::new();
 
     loop {
-        match listener.accept().await {
-            Ok((mut socket, _addr)) => {
-                let store = store.clone();
-                tokio::spawn(async move {
-                    let (read_half, write_half) = socket.split();
-                    let mut conn = Connection::new(read_half, write_half, store);
-                    if let Err(e) = conn.run().await {
-                        println!("connection failed: {:?}", e)
+        select! {
+            connection_result = listener.accept() => {
+                match connection_result {
+                    Ok((mut socket, _addr)) => {
+                        let store = store.clone();
+                        open_connections.spawn(async move {
+                            let (read_half, write_half) = socket.split();
+                            let mut conn = Connection::new(read_half, write_half, store);
+                            if let Err(e) = conn.run().await {
+                                println!("connection failed: {:?}", e)
+                            }
+                        });
                     }
-                });
+                    Err(e) => println!("client couldn't connect: {:?}", e),
+                };
+            },
+            join_result = open_connections.join_next(), if !open_connections.is_empty() => {
+                if let Some(Err(err)) = join_result {
+                    println!("connection task failed: {:?}", err);
+                }
             }
-            Err(e) => println!("client couldn't connect: {:?}", e),
         }
     }
 }
