3b9d724d3b66304141130002261bf22b45b22816
Colin Cummins <all.our.august.sundays@gmail.com>
2026-03-16 13:21:07 -0700

    Shutdown scaffolding in main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use redlike::server::run_server;
+use tokio::signal::{self};
+use tokio_util::sync::CancellationToken;
 const ADDR: &str = "127.0.0.1:6379";
 
 #[tokio::main]
 #[allow(unused_variables)]
 async fn main() -> Result<(), std::io::Error> {
+    let shutdown = CancellationToken::new();
     let (_address, handle) = run_server(ADDR).await?;
-    handle.await?
+
+    match signal::ctrl_c().await {
+        Ok(()) => {
+            shutdown.cancel();
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
 }
