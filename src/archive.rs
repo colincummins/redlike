d3922c4cf14c94d1de17cc0f2cf50aca1e6e848d
Colin Cummins <all.our.august.sundays@gmail.com>
2026-03-25 14:59:25 -0700

    Implement testing for Archive::load (still need round-trip testing once Archive::save is ready)

--- src/archive.rs
+++ src/archive.rs
@@ -1,6 +1,5 @@
 use crate::store::RestoreError;
 use crate::store::Store;
-use std::error;
 use std::{fmt, path::PathBuf};
 use tokio::fs;
 
@@ -44,5 +43,55 @@ pub async fn load(path: PathBuf) -> Result<Store, ArchiveError> {
             }
         }
         Err(error) => Err(ArchiveError::ReadFile(error)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::ErrorKind;
+    use std::io::Write;
+    use std::path::PathBuf;
+
+    use tempfile::{NamedTempFile, TempDir};
+    use tokio::fs::File;
+
+    use crate::archive::{ArchiveError, load};
+    #[tokio::test]
+    async fn missing_file_with_relative_filename_returns_new_store() {
+        let file_path = PathBuf::new().join("test-archive");
+        let store = load(file_path).await.unwrap();
+        assert!(store.get(&b"missing-key".to_vec()).await.is_none());
+    }
+
+    #[tokio::test]
+    async fn missing_filename_in_existing_dir_returns_new_store() {
+        let temp_dir = TempDir::new().unwrap();
+        let file_path = temp_dir.path().join("test-archive");
+        let store = load(file_path).await.unwrap();
+        assert!(store.get(&b"missing-key".to_vec()).await.is_none());
+    }
+
+    #[tokio::test]
+    async fn file_with_bad_directory_path_returns_error() {
+        let file_path = TempDir::new()
+            .unwrap()
+            .path()
+            .join("does_not_exist")
+            .join("test-archive");
+        assert!(matches!(
+            load(file_path).await,
+            Err(ArchiveError::ReadFile(_))
+        ));
+    }
+
+    #[tokio::test]
+    async fn malformed_archive_returns_invalid_archive_error() {
+        let mut bad_archive = NamedTempFile::new().unwrap();
+        let bad_bytes = b"This is not an archive";
+        bad_archive.write_all(bad_bytes).unwrap();
+        assert!(matches!(
+            load(bad_archive.path().into()).await,
+            Err(ArchiveError::InvalidArchive(_))
+        ))
     }
 }
