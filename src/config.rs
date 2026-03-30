95a1a8108c6b3917153d71dbb9eee679ccc783fb
Colin Cummins <all.our.august.sundays@gmail.com>
2026-03-30 11:43:41 -0700

    Change to auth_password model instead of a file so we can use env/secrets instead of managing password file on app

--- src/config.rs
+++ src/config.rs
@@ -10,8 +10,8 @@ pub struct Config {
     pub port: u16,
     #[arg(short = 'r', long, env, default_value = None)]
     pub archive_path: Option<std::path::PathBuf>,
-    #[arg(long, env, default_value = None)]
-    pub auth_password_file: Option<std::path::PathBuf>,
+    #[arg(long, env = "AUTH_PASSWORD", hide_env_values = true)]
+    pub auth_password: Option<String>,
 }
 
 pub fn get_config() -> Config {
