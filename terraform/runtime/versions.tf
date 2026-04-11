c55f257771536f879a4803dc988b18516fa2ae30
Colin Cummins/usr/local/bin/python3 <84826067+colincummins@users.noreply.github.com>
2026-04-10 20:43:39 -0700

    Working branch for terraform deployment/teardown of full runtime

---
+++ terraform/runtime/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "1.14.8"
+
+  cloud {
+    organization = "ColinCumminsCS"
+
+    workspaces {
+      name = "redlike-runtime"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
